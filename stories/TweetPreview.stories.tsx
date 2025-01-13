e8e1603ab43d0bb0d874570f8aad1d7a3707b28c
Pavel Prokudin <paul.pro@ya.ru>
2025-01-14 00:18:58 +0100

    feat: add types to a build

--- stories/TweetPreview.stories.tsx
+++ stories/TweetPreview.stories.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import { TweetPreview } from '../src/TweetPreview';
 import { TweetErrorBoundary } from '../src/TweetErrorBoundary';
